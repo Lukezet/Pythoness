b716589b135f81374a1270b9cdb1179778df4c06
lukezet <lucasyanezbogni@hotmail.com>
2022-04-25 18:11:11 -0300

    playing2

--- practica10.py
+++ practica10.py
@@ -1,10 +1,18 @@
+"""def run():
+
+    pote[i**2 for i in range(1,101) if i % 3 != 0]  
+
+    print(pote)
+
+if __name__=="__main__":
+    run()"""
 def run():
-    lista=[1,2,3,4,5,6,7,8,9,10]
-    resultado=[]
-    pote=0
-    for i in lista:
-        pote=i**2
-        resultado.append(pote)
-    print(resultado)
+    """pote=[]
+    for i in range(1,101):
+        if i%3!=0:
+            pote.append(i**2)"""
+    pote=[i for i in range(1,100000) if (i % 4 == 0 and i % 6 == 0 and i % 9 == 0)]        
+    
+    print(pote)
 if __name__=="__main__":
     run()
